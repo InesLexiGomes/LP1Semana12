ebb15fdb292df31c484f66b00387ba2a7478e4f4
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-05-31 13:23:11 +0100

    created the IsPalindrome method

---
+++ Palindrome/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        bool IsPalindrome(string s)
+        {
+            bool PalindromeChecker()
+            {
+                // Convert to array to be reversed
+                Char[] chars = s.ToCharArray();
+
+
+                // Create new string to be reversed
+                string newString = "";
+
+                // For each character put it before the new string
+                foreach (char c in chars)
+                {
+                    newString = $"{c}{newString}";
+                }
+
+                // if the string equals the string reversed equals true
+                if (newString == s) return true;
+                else return false;
+            }
+
+            // if there's nothing throw an exception
+            if (s == "") throw new ArgumentNullException("Empty String");
+            
+            // if it's just one character it's always a Palindrome
+            else if (s.Length < 2) return true;
+
+            else
+            {
+                return PalindromeChecker();
+            }
+
+        }
+    }
+}
